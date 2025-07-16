159d9bbbef98ef60c0daaa3415ccfde61511c0df
Duy Dao <daothanhduy305@gmail.com>
2025-07-16 08:09:14 +0700

    feat: add application version and commit hash display in UI, update CI workflow for metadata handling

---
+++ CaddyManager/Configurations/Application/ApplicationInfo.cs
@@ -0,0 +1,17 @@
+namespace CaddyManager.Configurations.Application;
+
+/// <summary>
+/// Containing information about the application
+/// </summary>
+public class ApplicationInfo
+{
+    /// <summary>
+    /// The version of the application, to be defined and tagged 
+    /// </summary>
+    public static readonly string Version = "1.0.0";
+
+    /// <summary>
+    /// The commit hash of the application
+    /// </summary>
+    public static readonly string CommitHash = "[DEVELOPMENT]";
+}
